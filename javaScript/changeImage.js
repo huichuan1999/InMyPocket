f31bde892f2c273ecc97bdd630de72edf772bbeb
huichuan1999 <58366430+huichuan1999@users.noreply.github.com>
2023-09-30 20:00:32 +0100

    add file

--- javaScript/changeImage.js
+++ javaScript/changeImage.js
@@ -6,15 +6,24 @@
 //         background.style.backgroundImage = "url('./images/BG1.jpg')";
 //     }
 // }
+// function changeBg(hover) {
+//     var bg1 = document.getElementById('bg1');
+//     var bg2 = document.getElementById('bg2');
+//     if(hover) {
+//         bg1.style.opacity = '0';
+//         bg2.style.opacity = '1';
+//     } else {
+//         bg1.style.opacity = '1';
+//         bg2.style.opacity = '0';
+//     }
+// }
+
 function changeBg(hover) {
-    var bg1 = document.getElementById('bg1');
-    var bg2 = document.getElementById('bg2');
+    var background = document.getElementById('bg2');
     if(hover) {
-        bg1.style.opacity = '0';
-        bg2.style.opacity = '1';
+        background.style.opacity = '1';
     } else {
-        bg1.style.opacity = '1';
-        bg2.style.opacity = '0';
+        background.style.opacity = '0';
     }
 }
 
